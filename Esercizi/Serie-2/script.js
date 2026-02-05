7c26e4e4223d18d7e748dd672453fa4bddd476a0
Nik <nik1.inginf@gmail.com>
2026-02-05 16:27:02 +0100

    Series 0-1-2
    05.02.2026 javascript series.

---
+++ Esercizi/Serie-2/script.js
@@ -0,0 +1,51 @@
+// Nicola Galeano 
+// version: 05.02.2026
+// Serie 2
+
+// Parte 1
+
+// 1.1
+for (let i = 1; i <= 10; i++) {
+    console.log(i); 
+}
+// 1.2
+for (let i = 1; i <= 10; i++) {
+    console.log(5 * i); 
+}
+// 1.3
+for (let i = 10; i >= 1; i--) {
+    console.log(i); 
+}
+
+// Parte 2
+
+//2.1
+let a = 1;
+while (a < 6) {
+    console.log(a);
+    a++;
+}
+
+// 2.2
+somma = 0;
+a = 1;
+while (a <= 100) {
+    somma +=a;
+    a++;
+}
+console.log(somma);
+
+// Parte 3
+
+
+// Parte 4
+
+
+// Parte 5
+
+
+// Parte 6
+
+
+// Parte 7
+
